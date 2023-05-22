54568a1af3ab54ac9e9ddad2457d6c74c7887cd2
Carlos Henrique <124469763+Carl6x@users.noreply.github.com>
2023-05-22 12:26:05 -0300

    teste console

--- js/script.js
+++ js/script.js
@@ -2,41 +2,91 @@ const camposDoFormulario = document.querySelectorAll('[required]');
 const submitButton = document.querySelectorAll('.input-submit');
 // console.log(submitButton);
 
+function validaCnpj(){
+    if (camposDoFormulario[0].value.lenght<18){
+        console.log("CNPJ INVÁLIDO.")
+    }else{
+        console.log("CNPJ válido.")
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // submitButton.addEventListener("submit", (e) => {
 //     e.preventDefault();
 
 // })
 
-const typesOfError = [
-    "valueMissing",
-    "typeMismatch",
-    "patternMismatch",
-    "tooShort",
-    "customError",
-    "tooLong",
+// const typesOfError = [
+//     "valueMissing",
+//     "typeMismatch",
+//     "patternMismatch",
+//     "tooShort",
+//     "customError",
+//     "tooLong",
 
-]
+// // ]
 
-const mensagensDeErro = {
+// const mensagensDeErro = {
 
-    cnpj: {
-        tooLong:"CNPJ inválido",
-        },
+//     cnpj: {
+//         tooLong:"CNPJ inválido",
+//         },
         
-    razao: { 
-        tooLong: "Tamanho máximo de 30 caracteres",
-    },
+//     razao: { 
+//         tooLong: "Tamanho máximo de 30 caracteres",
+//     },
 
-    cpf: {
-        tooLong: " CPF inválido"
-    },
+//     cpf: {
+//         tooLong: " CPF inválido"
+//     },
 
-    nome: {
-        tooLong: "Tamanho máximo de 20 caracteres"
-    },
+//     nome: {
+//         tooLong: "Tamanho máximo de 20 caracteres"
+//     },
 
-    nickname: {
-        tooLong: "Tamnho máximo de 10 caracteres"
-    }
+//     nickname: {
+//         tooLong: "Tamnho máximo de 10 caracteres"
+//     }
 
-}
+// }
